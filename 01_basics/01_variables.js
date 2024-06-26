322f36eedeb45b79203f713c2c5dd489d4a87b47
parassachdeva0111 <125549878+parassachdeva0111@users.noreply.github.com>
2024-06-26 06:22:10 +0000

    basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 1234;
+let accountEmail = "paras@gmail.com";
+var accountPassword = "1234";
+accountCity = "Punjab";
+let accountState;
+
+// accountId = 12;
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]);
+
+accountEmail = "mohit@gmail.com";
+accountPassword = "4321";
+accountCity = "Haryana"
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState]);
