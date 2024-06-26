a598390d1e74f5a1e9a69a3e1a4389b2435075a6
parassachdeva0111 <125549878+parassachdeva0111@users.noreply.github.com>
2024-06-26 18:07:44 +0000

    Lecture 3

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score = "33abc";
+let score1 = null;
+let score2 = undefined;
+
+
+// console.log(typeof score);
+// console.log(typeof score1);
+// console.log(typeof score2);
+
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// console.log(typeof score1);
+
+// let valueInNumber1 = Number(score1);
+// console.log(typeof valueInNumber1);
+// console.log(valueInNumber1);
+
+
+// let valueInNumber2 = Number(score2);
+// console.log(typeof valueInNumber2);
+// console.log(valueInNumber2);
+
+let isLoggedIn = 1;
+let isLoggedIn1 = "";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+let booleanIsLoggedIn1 = Boolean(isLoggedIn1);
+
+
+console.log(booleanIsLoggedIn);
+console.log(booleanIsLoggedIn1);
+
+
+// 1 => tree ;
+// 0 => false;
+// "" => false;
+// "Paras" => true;
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
+
+
