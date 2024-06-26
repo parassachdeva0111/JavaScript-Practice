48bad68794ce24464eafd0a56a9d807867f8fa59
parassachdeva0111 <125549878+parassachdeva0111@users.noreply.github.com>
2024-06-26 06:55:32 +0000

    lecture 2 Datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+let Name = "Paras";
+let age = 21;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53;
+// BigInt
+// string = "";
+// Boolean = true/false;
+// null = standalone value;
+// undefined
+// Symbol => unique;
+
+// object
+console.log(Name);
+
+console.log(typeof (Name));
+
+console.table([typeof Name ,typeof age ,typeof isLoggedIn , typeof state]);
+
+console.log(typeof undefined);
+
+console.log(typeof null);
+
