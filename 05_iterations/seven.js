da4574889e1d665c6ef20d6ffb04bb257665e207
parassachdeva0111 <125549878+parassachdeva0111@users.noreply.github.com>
2024-08-27 09:21:48 +0000

    Just doing work

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,10 +1,26 @@
-const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-// const newNums = myNumers.map( (num) => { return num + 10})
+// // const newNums = myNumers.map( (num) => { return num + 10})
 
-const newNums = myNumers
-                .map((num) => num * 10 )
-                .map( (num) => num + 1)
-                .filter( (num) => num >= 40)
+// const newNums = myNumers
+//                 .map((num) => num * 10 )
+//                 .map( (num) => num + 1)
+//                 .filter( (num) => num >= 40)
 
-console.log(newNums);
+// console.log(newNums);
+
+const myNumber = [1,2,3,4,5,6,7,8,9,10];
+
+const newNum = myNumber.map((num) => {return num + 10});
+
+
+const newNum1 = myNumber
+                        .map((num) =>  num * 10)
+                        .map((num) => num + 1)
+                        .filter((num) => num >= 41)
+
+// console.log(myNumber);
+// console.log(newNum);
+console.log(newNum1);
+
+
